b32955f07e1c796963a18ef122fc2a2309469295
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-08-29 11:08:03 +0530

    backend services are written in appwrite

---
+++ src/appwrite/config.js
@@ -0,0 +1,122 @@
+import config from "../config/config";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userID }) {
+        try {
+            return await this.databases.createDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug, {
+                title,
+                content,
+                featuredImage,
+                status,
+                userID
+            })
+        } catch (error) {
+            console.log("Appwrite service:: createPost::error", error);
+
+        }
+    }
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug, {
+                title,
+                content,
+                featuredImage,
+                status,
+            })
+        } catch (error) {
+            console.log("Appwrite service:: updatePost::error", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+
+            return true
+        } catch (error) {
+            console.log("Appwrite service:: deletePost::error", error);
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service:: getPost::error", error);
+        }
+    }
+
+    async getAllPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+
+            )
+        } catch (error) {
+            console.log("Appwrite service:: getAllPosts::error", error);
+            return false;
+        }
+    }
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service:: uploadFile::error", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            return await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service:: deleteFile::error", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId,
+
+        )
+    }
+
+}
+
+const service = new Service()
+export default service;
---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_COLLECTION_ID)
+}
+
+export default config
