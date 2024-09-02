e25e6fd8ac4717400cfd58c0ec463e517b2e0e1a
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-09-02 19:51:36 +0530

    defined the structue of project with header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-          TODO: <Outlet />
+           <Outlet />
         </main>
         <Footer />
         </div>
